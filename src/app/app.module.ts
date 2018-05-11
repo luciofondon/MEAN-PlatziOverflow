c5bfc6ebe45511f1322291cb7f51765c5b3928fb
Lucio David Fondón Terrón <luciofondon@MacBook-Pro-de-Lucio.local>
2018-05-12 00:07:45 +0200

    Creando componente question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+
+//Material Angular
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module'
+import 'hammerjs';
+
+import { QuestionDetailComponent } from './question/question-detail.component';
+
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules,
+})
+export class MaterialModule { }
